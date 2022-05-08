4a5236a1861140fbfdfb4a0b4ac2bbfb3ea44a98
Chris Back <chrisback011996@gmail.com>
2022-05-08 16:59:47 +1000

    Partially finished show view page. Added in details, brands, routes

--- app/controllers/reels_controller.rb
+++ app/controllers/reels_controller.rb
@@ -1,2 +1,12 @@
 class ReelsController < ApplicationController
+
+  def index
+    @reels = Reel.all
+  end
+  
+  def show
+    @reel = Reel.find(params[:id])
+  end
+
+
 end
--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,3 +1,7 @@
 class Brand < ApplicationRecord
   has_many :reels
+
+  def brand
+    return "#{brand}"
+  end
 end
