48f889b1569415827218d455be71aafd61864ee5
Chris Back <chrisback011996@gmail.com>
2022-05-20 00:43:45 +1000

    Debug heroku

--- app/controllers/wish_lists_controller.rb
+++ app/controllers/wish_lists_controller.rb
@@ -1,18 +1,18 @@
-class WishListsController < ApplicationController
+# class WishListsController < ApplicationController
 
-  def index
-    WishList.all
-  end
+#   def index
+#     WishList.all
+#   end
 
-  def create
-    WishList.create!(reel: Reel.find(params[:id]), profile: current_user.profile)
-    redirect_to reels_path
-  end
+#   def create
+#     WishList.create!(reel: Reel.find(params[:id]), profile: current_user.profile)
+#     redirect_to reels_path
+#   end
 
 
-  def destroy
-    destroy_reel = Reel.find(params[:id])
-    WishList.destroy(reel: Reel.find(params[:id]), profile: current_user.profile)
-    redirect_to reels_path
-  end
-end
+#   def destroy
+#     destroy_reel = Reel.find(params[:id])
+#     WishList.destroy(reel: Reel.find(params[:id]), profile: current_user.profile)
+#     redirect_to reels_path
+#   end
+# end
